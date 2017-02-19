69d53a9ef9bca393c0a324c2fa943daae105c94f
Mickey Vellukunnel <mickey.vellukunnel@gmail.com>
2017-02-19 14:31:06 -0500

    Class to store individual peer info.

---
+++ src/peer/PeerInfo.java
@@ -0,0 +1,52 @@
+/**
+ * 
+ */
+package peer;
+
+/**
+ * @author Mickey Vellukunnel (mickeymv)
+ *	Class which encapsulates details about a peer such as 
+ *	ID, hostname, portnumber, whether it has the initial file or not, BitField, etc.
+ */
+public class PeerInfo {
+	
+	private String peerID;
+	private String hostName;
+	private int portNumber;
+	boolean hasFileInitially;
+	
+	public PeerInfo(String peerID, String hostName, int portNumber, boolean hasFileInitially) {
+		this.peerID = peerID;
+		this.hostName = hostName;
+		this.portNumber = portNumber;
+		this.hasFileInitially = hasFileInitially;
+	}
+	
+	public String getPeerID() {
+		return peerID;
+	}
+	public void setPeerID(String peerID) {
+		this.peerID = peerID;
+	}
+	public String getHostName() {
+		return hostName;
+	}
+	public void setHostName(String hostName) {
+		this.hostName = hostName;
+	}
+	public int getPortNumber() {
+		return portNumber;
+	}
+	public void setPortNumber(int portNumber) {
+		this.portNumber = portNumber;
+	}
+	public boolean isHasFileInitially() {
+		return hasFileInitially;
+	}
+	public void setHasFileInitially(boolean hasFileInitially) {
+		this.hasFileInitially = hasFileInitially;
+	}
+	
+	
+
+}
