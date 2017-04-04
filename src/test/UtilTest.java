0be64cf42e64967469f3a3146d8c5547d05120da
Mickey Vellukunnel <mickey.vellukunnel@gmail.com>
2017-04-04 10:52:07 -0400

    Merge conflict correction for the UtilTest file.

--- src/test/UtilTest.java
+++ src/test/UtilTest.java
@@ -11,26 +11,28 @@ public class UtilTest {
 
 	@Test
 	public void test() {
-//		fail("Not yet implemented");
-		
-//		Byte b = Util.setFirstNDigits(3);
-//		String s1 = String.format("%8s", Integer.toBinaryString(b & 0xFF)).replace(' ', '0');
-//		System.out.println(s1);
-//		
-//		int n = Util.getFirstNDigits(new Byte((byte)0XF8));
-//		System.out.println(n);
+		// fail("Not yet implemented");
+
+		// Byte b = Util.setFirstNDigits(3);
+		// String s1 = String.format("%8s", Integer.toBinaryString(b &
+		// 0xFF)).replace(' ', '0');
+		// System.out.println(s1);
+		//
+		// int n = Util.getFirstNDigits(new Byte((byte)0XF8));
+		// System.out.println(n);
 		Util inst = Util.getInstance();
 		ConfigurationSetup instance = ConfigurationSetup.getInstance();
-		
+
 		Util.initiateBitfield();
-		Util.createRandomDataFile(instance.getFileSize());
-		Util.splitDataFile();
-		
-		
-		//System.out.println(ConfigurationSetup.numberOfPieces);
-		//Util.printBitfield();
-		
-		
+		// Util.createRandomDataFile(instance.getFileSize());
+		// Util.splitDataFile();
+		// Util.mergeDataPieces("project/");
+		// System.out.println(ConfigurationSetup.numberOfPieces);
+		// Util.printBitfield();
+		byte[] data = Util.getPieceAsByteArray(1);
+		for (byte b : data) {
+			Util.printByteToBinaryString(b);
+		}
 	}
 
 }
