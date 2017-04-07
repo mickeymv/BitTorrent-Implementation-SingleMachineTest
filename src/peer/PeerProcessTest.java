005b834f183381a3a51dac0c8230cf4efd4cc74e
Xiaolong Li <xiaolongl@ufl.edu>
2017-04-07 14:06:56 -0400

    peer.peerProcessTest added

---
+++ src/peer/PeerProcessTest.java
@@ -0,0 +1,53 @@
+package peer;
+
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import connection.TCPConnectionManager;
+import logging.FileLogger;
+import peer.PeerProcess;
+import type.PeerInfo;
+import util.ConfigurationSetup;
+import util.Util;
+
+/**
+ * @author Xiaolong Li
+ */
+
+public class PeerProcessTest {
+	
+	PeerProcess peerProcess = null;
+	@Before
+	public void pretest() {
+		
+		Util inst = Util.initializeUtil();
+		ConfigurationSetup instance = ConfigurationSetup.getInstance();
+		
+		peerProcess = new PeerProcess("2");
+	}
+	
+	
+	@Test
+	public void test() {
+		
+		System.out.println("local peer id: " + peerProcess.getLocalPeerID());
+		
+		// print the size of neighbors
+		System.out.println("size of neighbors: " 
+				+ peerProcess.getNeighbors().size());
+		for (PeerInfo peer : peerProcess.getNeighbors()) {
+			
+			System.out.println(peer.getPeerID());
+		}
+		assertTrue(1 == peerProcess.getNeighbors().size());
+		
+		
+		
+	}
+
+
+}
